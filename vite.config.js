8bc625c74589d1f55ae125de016986e5c2aa166f
Doni Wahyudi <doni.wahyudi@btpnsyariah.com>
2026-02-23 13:31:08 +0700

    fix(router): add 404.html fallback duplicate for SPA routing on github pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        {
+            name: 'create-404',
+            writeBundle(options) {
+                const indexHtml = path.resolve(options.dir, 'index.html');
+                const p404Html = path.resolve(options.dir, '404.html');
+                if (fs.existsSync(indexHtml)) {
+                    fs.copyFileSync(indexHtml, p404Html);
+                }
+            }
+        }
+    ],
     base: '/barberpage/',
 })
