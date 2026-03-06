27606e1a54e0de0750e32eb5a86d54f6776cdf0a
Doni Wahyudi <doni.wahyudi@btpnsyariah.com>
2026-03-06 20:14:30 +0700

    update navbar

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,15 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+// Disconnect any ghost Service Workers left over from other localhost projects
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function (registrations) {
+    for (let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
